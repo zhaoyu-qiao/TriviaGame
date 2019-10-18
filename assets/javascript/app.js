c9e5aa5e4095bb842376e9097457b8c99ff765d0
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-10-18 14:33:54 -0400

    Add correct message and wrong message, add timeup function to clear interval, add label to radio button, line 54 and line 119 still has issue, it may be related to index calculation, also may need to use .html instead of .append to replace content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,172 @@
 //when dom is loaded, do the below:
 //reference the stopwatch js to call the event listener and functions at the top
-window.onload = function() {
-  $("#start").on("click", start);
-  $("#next").on("click", next);
-  $("#restart").on("click", restart);
+window.onload = function () {
+    $("#start").on("click", start);
+    $("#next").on("click", next);
+    $("#restart").on("click", restart);
 };
 $("#questions").hide();
 
-let questionsArray = ["question1", "question2", "quetion3", "question4"];
+/*let questionsArray = ["question1", "question2", "quetion3", "question4"];
 let answersArray = [
   { answer0: ["option1", "option2", "option3", "option4"] },
   { answer1: ["option1", "option2", "option3", "option4"] },
   { answer2: ["option1", "option2", "option3", "option4"] },
   { answer3: ["option1", "option2", "option3", "option4"] }
-];
+];*/
+
+let currentQuestionIndex = 0;
+let correctAnswersCount = 0;
+let incorrectAnswersCount = 0;
+
+let questions = [{
+    question: "what is the color of sky?",
+    answers: ["kjhjkh", "dsaf", "dgfdzg", "dsgdsg"],
+    correctAns: "dsaf",
+    wrongImage: "",
+    correctImage: ""
+}, {
+    question: "what is the color of cat?",
+    answers: ["kjhjkh", "dsaf", "dgfdzg", "dsgdsg"],
+    correctAns: "dsaf",
+    wrongImage: "",
+    correctImage: ""
+}, {
+    question: "what is the color of banana?",
+    answers: ["kjhjkh", "dsaf", "dgfdzg", "dsgdsg"],
+    correctAns: "dsaf",
+    wrongImage: "",
+    correctImage: ""
+}, {
+    question: "what is the color of apple?",
+    answers: ["kjhjkh", "dsaf", "dgfdzg", "dsgdsg"],
+    correctAns: "dsaf",
+    wrongImage: "",
+    correctImage: ""
+}];
 let time = 10;
 let clockRunning = false;
 
 //for (i = 0; i < 20; i++) {
 //    var radioBtn = $('<input type="radio" name="rbtnCount" />');
 //    radioBtn.appendTo('#target');
-function writeQuestions(question) {
-  $("#question").append(question);
+function writeQuestions(current) {
+    console.log("current in write Question function: " + questions[current].question);
+    $("#question").append(questions[current].question);
+    //writeOptions(current);
+    console.log('current question: ' + questions[current].question);
 }
 
-function writeOptions(options) {
-  let radioBtn = $('<input type="radio" name="rbtnCount" />');
-  radioBtn.appendTo("#questions");
-}
-function timer(time) {
-  setInterval(function() {
-    if (!clockRunning && time > 0) {
-      time--;
-      console.log(time);
+function writeOptions(index) {
+    console.log("write options function");
+    for (let i = 0; i < index.answers.length; i++) {
+        console.log(index.answers[i]);
+        let radioBtn = $(
+            '<label>' + index.answers[i] + '</label><input type="radio" name="options" class="radio-btn" value="' +
+            index.answers[i] +
+            '"/><br/>'
+        );
+        console.log(radioBtn);
+        $(".options").append(radioBtn);
     }
-  }, 1000);
-  return;
+    //   '<input>' + " 'value"
+    //   let radioBtn = $('<input type="radio" name="rbtnCount" />');
+    //   radioBtn.appendTo("#questions");
+}
+
+function timer(time) {
+    let timer = setInterval(function () {
+        if (!clockRunning && time > 0) {
+            time--;
+            console.log(time);
+        }
+        if (time === 0) {
+            console.log("time up");
+            timeup();
+        }
+    }, 1000);
+    return;
+}
+
+function timeup() {
+    clearInterval(timer);
+
 }
 
 function start() {
-  //hide the start and restart button, show the questions form
-  $("#start").hide();
-  $("#restart").hide();
-  $("#questions").show();
+    if (currentQuestionIndex > 0) {
+        return;
+    } else {
+        //hide the start and restart button, show the questions form
+        $("#start").hide();
+        $("#restart").hide();
+        $("#questions").show();
 
-  //start timer
-  timer(time);
-  writeQuestions(questionsArray[0]);
-  writeOptions(answersArray.answer0);
+        //start timer
+        timer(time);
+        writeQuestions(currentQuestionIndex);
+        writeOptions(questions[currentQuestionIndex]);
+    }
+
+    //writeOptions(answersArray.answer0);
 }
 
-if (time === 0) {
-  next();
+//$(this)
+// $(".radio-btn").on("click", function (event) { //use this if your browser cannot find radio btn by class.
+$(document).on("click", ".radio-btn", function (event) {
+    //clearInterval(timer);
+    console.log("user answer : " + $(event.target).attr("value"));
+    if ($(event.target).val() === questions[currentQuestionIndex].correctAns) {
+        answeredCorrect()
+    } else {
+        answeredWrong();
+    }
+    next();
+})
+
+function answeredCorrect() {
+    console.log("correct");
+    //add images and correct answser
+    clearInterval(timer);
+
+
+    if (currentQuestionIndex === questions.length - 1) {
+        setTimeout(results(), 3000);
+    } else {
+        setTimeout(next(), 3000)
+    }
+    correctAnswersCount++;
 }
+
+function answeredWrong() {
+    console.log("wrong");
+    clearInterval(timer);
+
+    if (currentQuestionIndex === questions.length - 1) {
+        setTimeout(results(), 3000);
+    } else {
+        setTimeout(next(), 3000)
+    }
+    incorrectAnswersCount++;
+}
+
+// if (time === 0) {
+//     next();
+// }
 
 //user for loop for the next function? checkout the slideshow activity
+//need to make sure when next is clicked, start no longer execute?????
+//do i neeed to write everthing within the for loop?
+
 function next() {
-  time = 10;
-  $("#start").hide();
-  $("#restart").hide();
-  $("#questions").show();
-  timer(time);
-  writeQuestions(questionsArray[1]);
-  writeOptions(answersArray.answer1);
+    time = 10;
+    $("#start").hide();
+    $("#restart").hide();
+    $("#questions").show();
+    timer(time);
+    currentQuestionIndex++;
+    writeQuestions(currentQuestionIndex);
+    writeOptions(questions[currentQuestionIndex]);
 }
 
 //Show the question and its selections with checkboxes
---
+++ assets/javascript/object.js
@@ -0,0 +1,66 @@
+const obj = [
+  {
+    _id: "5da8e9a88c42895122ce9a8f",
+    index: 0,
+    guid: "e9f35c33-e3b9-4ff6-9f4a-89c11931ccc7",
+    isActive: false,
+    balance: "$2,550.93",
+    picture: "http://placehold.it/32x32",
+    age: 21,
+    eyeColor: "brown",
+    name: "Cummings Clarke",
+    gender: "male",
+    company: "PRINTSPAN",
+    email: "cummingsclarke@printspan.com",
+    phone: "+1 (870) 468-2177",
+    address: "386 Murdock Court, Loyalhanna, Arizona, 5977",
+    registered: "2016-04-22T06:28:02 +04:00",
+    latitude: 40.764755,
+    longitude: -66.704756,
+    tags: ["commodo", "commodo"],
+    friends: [
+      {
+        id: 0,
+        name: "Whitfield Newman"
+      },
+      {
+        id: 1,
+        name: "Stacie Avila"
+      }
+    ],
+    greeting: "Hello, Cummings Clarke! You have 6 unread messages.",
+    favoriteFruit: "strawberry"
+  },
+  {
+    _id: "5da8e9a85af4c67418c28741",
+    index: 1,
+    guid: "57ad166e-0175-4ade-b383-b3ddf34329e7",
+    isActive: true,
+    balance: "$2,605.21",
+    picture: "http://placehold.it/32x32",
+    age: 36,
+    eyeColor: "blue",
+    name: "Obrien Sharpe",
+    gender: "male",
+    company: "EDECINE",
+    email: "obriensharpe@edecine.com",
+    phone: "+1 (806) 581-3576",
+    address: "640 Ridgecrest Terrace, Brethren, Massachusetts, 4292",
+    registered: "2019-02-20T01:41:15 +05:00",
+    latitude: 87.759743,
+    longitude: 120.197759,
+    tags: ["aute", "fugiat"],
+    friends: [
+      {
+        id: 0,
+        name: "Consuelo Mills"
+      },
+      {
+        id: 1,
+        name: "Mildred Underwood"
+      }
+    ],
+    greeting: "Hello, Obrien Sharpe! You have 2 unread messages.",
+    favoriteFruit: "apple"
+  }
+];
