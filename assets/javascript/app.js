7a094283d83afcee28bef9c4a3f1461478f1dfe8
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-10-20 16:16:35 -0400

    add styling to css and images to the js file, there's something not working with the comparing logic or index++ logic that needs to be figured out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,18 +4,19 @@ window.onload = function () {
     $("#start").on("click", start);
     $("#next").on("click", next);
     $("#restart").on("click", restart);
+    $("#questions").hide();
+    $("#results").hide();
+    $("#next").hide();
+    $("#restart").hide();
 };
-$("#questions").hide();
-$("#results").hide();
+// $("#questions").hide();
+// $("#results").hide();
+// $("#next").hide();
+// $("#restart").hide();
 
-/*let questionsArray = ["question1", "question2", "quetion3", "question4"];
-let answersArray = [
-  { answer0: ["option1", "option2", "option3", "option4"] },
-  { answer1: ["option1", "option2", "option3", "option4"] },
-  { answer2: ["option1", "option2", "option3", "option4"] },
-  { answer3: ["option1", "option2", "option3", "option4"] }
-];*/
 
+
+//Define variables
 let currentQuestionIndex = 0;
 let correctAnswersCount = 0;
 let incorrectAnswersCount = 0;
@@ -24,31 +25,68 @@ let time = 10;
 let clockRunning = false;
 let countDown;
 
-
+//let questions be in an array of objects, each object contain its own question, options and answers.
 let questions = [{
-    question: "what is the color of sky?",
-    answers: ["kjhjkh", "blue", "dgfdzg", "dsgdsg"],
-    correctAns: "blue",
-    wrongImage: "",
-    correctImage: ""
+    question: 'Which English guitarist has the nickname "Slowhand"',
+    answers: ["Eric Clapton", "Mark Knopfler", "Jeff Beck", "Jimmy Page"],
+    correctAns: "Eric Clapton",
+    correctImage: "./assets/images/eric.png",
+    wrongImage: "./assets/images/nope.png",
 }, {
-    question: "what is the color of cat?",
-    answers: ["kjhjkh", "grey", "dgfdzg", "dsgdsg"],
-    correctAns: "grey",
-    wrongImage: "",
-    correctImage: ""
+    question: 'What is the best selling album of all time from 1976 to 2018?',
+    answers: ["Back in Black", "Abbey Road", "The Dark Side of the Moon", "Thriller"],
+    correctAns: "Thriller",
+    correctImage: "./assets/images/thriller.png",
+    wrongImage: "./assets/images/nope.png",
 }, {
-    question: "what is the color of banana?",
-    answers: ["kjhjkh", "yellow", "dgfdzg", "dsgdsg"],
-    correctAns: "yellow",
-    wrongImage: "",
-    correctImage: ""
+    question: 'Which brass instrument has the lowest pitch in an orchestra?',
+    answers: ["Trumpet", "Trombone", "Tuba", "Saxophone"],
+    correctAns: "Tuba",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/tuba.png"
 }, {
-    question: "what is the color of apple?",
-    answers: ["kjhjkh", "red", "dgfdzg", "dsgdsg"],
-    correctAns: "red",
-    wrongImage: "",
-    correctImage: ""
+    question: 'What is the stage name of English female rapper Mathangi Arulpragasam, who is known for the song "Paper Planes&quot"?',
+    answers: ["K.I.A.", "M.I.A.", "C.I.A.", "A.I.A."],
+    correctAns: "M.I.A.",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/mia.png"
+}, {
+    question: '"Doctor Jones", "Turn Back Time", and "Barbie Girl", were UK number ones for which Eurodance group?',
+    correctAns: "Aqua",
+    answers: ["Vengaboys", "Sash!", "Eiffel 65", "Aqua"],
+    correctAns: "Aqua",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/aqua.png"
+}, {
+    question: 'Whose albums included "Back in Black" and "Ballbreaker"?',
+    answers: ["Iron Maiden", "AC/DC", "Black Sabbath", "Metallica"],
+    correctAns: "AC/DC",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/acdc"
+}, {
+    question: 'What was the subject of the 2014 song "CoCo" by American rapper O. T. Genasis?',
+    answers: ["Conan Brien", "Cobalt(II) carbonate", "Cocaine", "Coconut cream pie"],
+    correctAns: "Cocaine",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/cocaine.png"
+}, {
+    question: "Which Beatles album does NOT feature any of the band members on it's cover?",
+    answers: ["The Beatles (White Album)", "Rubber Soul.", "Abbey Road", "Magical Mystery Tour"],
+    correctAns: "The Beatles (White Album)",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/white.png"
+}, {
+    question: 'Sting, the lead vocalist of The Police, primarily plays what instrument?',
+    answers: ["Drums", "Bass Guitar", "Guitar", "Keyboards"],
+    correctAns: "Bass Guitar",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/bass.png"
+}, {
+    question: 'Which group performs the song "Crash into Me"?',
+    answers: ["Destiny's Child", "Phish", "The Grateful Dead", "Dave Matthews Band"],
+    correctAns: "Dave Matthews Band",
+    wrongImage: "./assets/images/nope.png",
+    correctImage: "./assets/images/dave.png"
 }];
 
 
@@ -95,6 +133,8 @@ function timer(time) {
         if (!clockRunning && time > 0) {
             time--;
             console.log(time);
+            $("#time-left").html("Time left:  " + time)
+
         }
         if (time === 0) {
             clockRunning = false;
@@ -105,30 +145,36 @@ function timer(time) {
     }, 1000);
 
     console.log('a timer:', countDown)
+    //$("#time-left").html("Time left:  " + time)
     return;
 }
 
 
 function start() {
+    // if (currentQuestionIndex > 0) {
+    //     return;
+    // } else {
+    //hide the start and restart button, show the questions form
+    $("#start").hide();
+    $("#restart").hide();
+    $("#questions").show();
+    $("#results").hide();
+    //Will only let user choose and then move on.
+    //$("#next").show();
 
+    //start timer
+    timer(time);
+    if (time === 0) {
 
-    if (currentQuestionIndex > 0) {
-        return;
+        next();
     } else {
-        //hide the start and restart button, show the questions form
-        $("#start").hide();
-        $("#restart").hide();
-        $("#questions").show();
-
-        //start timer
-        timer(time);
-        if (time === 0) {
-            next();
-        } else {
-            writeQuestions(currentQuestionIndex);
-            writeOptions(questions[currentQuestionIndex]);
-        }
+        //write content
+        writeQuestions(currentQuestionIndex);
+        writeOptions(questions[currentQuestionIndex]);
     }
+
+
+    //}
 
     //writeOptions(answersArray.answer0);
 }
@@ -139,9 +185,10 @@ function start() {
 $(document).on("click", ".radio-btn", function (event) {
     //clearInterval(timer);
     console.log("user answer : " + $(event.target).attr("value"));
+    console.log(questions[currentQuestionIndex].correctAns);
     //if ($(event.target).attr("value") === questions[currentQuestionIndex].correctAns) {
     //if ($(this).attr("value") === questions[currentQuestionIndex].correctAns) {
-    if ($(this).val() === questions[currentQuestionIndex].correctAns) {
+    if ($(event.target).attr("value") === questions[currentQuestionIndex].correctAns) {
         answeredCorrect()
         console.log("answer-chosen:correct");
     } else {
@@ -149,12 +196,16 @@ $(document).on("click", ".radio-btn", function (event) {
 
         //timer()
     }
+    //currentQuestionIndex++;
     next();
 })
 
 function answeredCorrect() {
     console.log("correct");
     //add images and correct answser
+    $("#answer").html("Correct!")
+    //$("#img").empty();
+    $("#img").attr("src", questions[currentQuestionIndex].correctImage);
     clearInterval(countDown);
 
 
@@ -169,6 +220,9 @@ function answeredCorrect() {
 
 function answeredWrong() {
     console.log("wrong");
+    $("#answer-block").html("Nope!")
+    //$("#img").empty();
+    $("#img").attr("src", questions[currentQuestionIndex].wrongImage);
     clearInterval(countDown);
 
     if (currentQuestionIndex === questions.length - 1) {
@@ -186,12 +240,13 @@ function answeredWrong() {
 //     next();
 // }
 
-//user for loop for the next function? checkout the slideshow activity
-//need to make sure when next is clicked, start no longer execute?????
-//do i neeed to write everthing within the for loop?
 
 function next() {
-    if (currentQuestionIndex === questions.length - 1) {
+    $("#next").show();
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        results();
         return;
     } else {
         time = 10;
@@ -199,16 +254,18 @@ function next() {
         $("#restart").hide();
         $("#questions").show();
         timer(time);
-        currentQuestionIndex++;
         writeQuestions(currentQuestionIndex);
         writeOptions(questions[currentQuestionIndex]);
     }
-    //if time =0, clear interval and move to next question.
 }
+//if time =0, clear interval and move to next question.
+
 
 function results() {
-    $("#questions").empty();
-    $("#options").empty();
+    $("#questions").hide();
+    $("#options").hide();
+    $("#time-left").hide();
+    $("#results").show();
     $("#correct").html("Correct Answers:" + correctAnswersCount);
     $("#incorrect").html("Wrong Answers:" + incorrectAnswersCount);
 
